e163967518acb08911cc97650fcf9f8e592f4b0b
awaaa <hawa.esanda@gmail.com>
2025-06-02 22:14:19 +0700

    Jobsheet6: Add favicon.ico file and update Layout component to include favicon link

--- website-pribadi/src/components/Layout.tsx
+++ website-pribadi/src/components/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: LayoutProps){
     return (
         <>
             <head>
-                <link rel="icon" href="/favicon.ico" />
+                 <link rel="icon" href="/favicon.ico" />
             </head>
             <Navbar />
             <main>{children}</main>
