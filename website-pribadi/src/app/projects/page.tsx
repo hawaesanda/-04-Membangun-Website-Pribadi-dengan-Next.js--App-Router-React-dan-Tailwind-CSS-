1b98adc1f7888948c1b3af964c63aaa5c0715900
awaaa <hawa.esanda@gmail.com>
2025-06-02 23:26:46 +0700

    Jobsheet4: Add new project images and update ProjectItem component for better display

--- website-pribadi/src/app/projects/page.tsx
+++ website-pribadi/src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 import type {Metadata } from 'next';
 import Image, { StaticImageData } from 'next/image';
-import image1 from '../../../public/images/project-1.jpg';
-import image2 from '../../../public/images/project-2.png';
+import image1 from '../../../public/images/p1.jpg';
+import image2 from '../../../public/images/p2.jpg';
 import { defaultMetadata } from '@/utils/metadata';
 
 export const metadata: Metadata = {
@@ -29,7 +29,12 @@ function ProjectItem({ name, url, urlDisplay, imageSrc }: ProjectItemProps) {
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
           <Image className="w-full" src={imageSrc} alt={name} />
           <div className="font-bold text-xl mb-2">
-
+            <div className="font-bold text-xl mb-2">{name}</div>
+          </div>
+          <div className="px-6 py-4">
+            {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              (urlDisplay)
+            </span> */}
           </div>
         </div>
       </a>
@@ -41,8 +46,26 @@ export default function Home(){
   return (
     <div className="mt-16 px-8">
       <header>
-        <h1 className="font-bold text-4x1 text-zinc-800">Proyek</h1>
+        <h1 className="font-bold text-4x1 text-zinc-800">Proyek Saya</h1>
+        <p className="text-base mt-6 text-zinc-600">Berikut adalah beberapa proyek yang telah saya kerjakan.</p>
       </header>
+      <div className="mt-16">
+        <h2 className="text-2xl">Aplikasi</h2>
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-12 gap-y-16 mt-8">
+          <ProjectItem 
+            name="Aplikasi 1"
+            url="https://example.com/project1"
+            urlDisplay="Lihat Proyek 1"
+            imageSrc={image1}
+          />
+          <ProjectItem 
+            name="Aplikasi 2"
+            url="https://example.com/project2"
+            urlDisplay="Lihat Proyek 2"
+            imageSrc={image2}
+          />
+        </ul>
+      </div>
     </div>
   );
 }
